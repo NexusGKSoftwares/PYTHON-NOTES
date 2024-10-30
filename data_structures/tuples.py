aa643367947987ea39179d81139a295bde007357
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-10-30 17:53:30 +0300

    "Added examples of tuple operations and methods in tuples.py"

--- data_structures/tuples.py
+++ data_structures/tuples.py
@@ -1,1 +1,36 @@
 # Tuples
+# 1. Creating a Tuple
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+# 2. Accessing Elements
+print(my_tuple[0])
+
+# 3. Tuple Unpacking
+a, b, c = my_tuple
+print(a, b, c)
+
+# 4. Single Element Tuple
+single_element = (5,)
+print(single_element)
+
+# 5. Checking Membership
+print(2 in my_tuple)
+
+# 6. Counting Elements
+print(my_tuple.count(1))
+
+# 7. Index of Element
+print(my_tuple.index(2))
+
+# 8. Converting Tuple to List
+my_list = list(my_tuple)
+print(my_list)
+
+# 9. Concatenating Tuples
+tuple_concat = my_tuple + (4, 5)
+print(tuple_concat)
+
+# 10. Nesting Tuples
+nested_tuple = ((1, 2), (3, 4))
+print(nested_tuple)
